db606ed397f65183f1198c9d59f9dcca7cc43015
ishworch <ishworchalise1@gmail.com>
2026-07-28 13:14:44 +0545

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="#home">
+        <a href="/home">
           Ishwor<span>.</span>
         </a>
       </div>
 
       <div className="nav-links">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/skills">Skills</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
       </div>
     </nav>
   );
