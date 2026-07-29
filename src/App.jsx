d3833c9a75819740130e42e3f55fb843aff9874d
ishworch <ishworchalise1@gmail.com>
2026-07-29 11:58:07 +0545

    css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Skills from './pages/Skills';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
